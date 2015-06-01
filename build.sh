cc0d33912c056dece897ca3e40044b3da763e6ee
darkangel-ua <to.darkangel@gmail.com>
2015-06-01 08:58:06 +0300

    should work for linux

--- build.sh
+++ build.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-wget https://dl.dropboxusercontent.com/u/13148925/hammmer-bootstrap-libs/libantlr3c-3.1.1.tar.bz2
-wget https://dl.dropboxusercontent.com/u/13148925/hammmer-bootstrap-libs/boost.guid-1.40.0.tar.bz2
-wget https://dl.dropboxusercontent.com/u/13148925/hammmer-bootstrap-libs/boost.crypto-1.40.0.tar.bz2
-wget https://dl.dropboxusercontent.com/u/13148925/hammmer-bootstrap-libs/boost.process-1.40.0.tar.bz2
+set -e
 
-tar -xf libantlr3c-3.1.1.tar.bz2 -C ./dependencies/libantlr3c
-tar -xf boost.guid-1.40.0.tar.bz2 -C ./dependencies/boost/guid
-tar -xf boost.crypto-1.40.0.tar.bz2 -C ./dependencies/boost/crypto
-tar -xf boost.process-1.40.0.tar.bz2 -C ./dependencies/boost/process
+function download()
+{
+   url=$1
+   filename=$2
+   unpack_dest=$3
 
+   wget -c "$url/$filename"
+   tar -xf "$filename" -C ./dependencies/$unpack_dest
+}
+
+download 'http://downloads.sourceforge.net/project/boost/boost/1.40.0' 'boost_1_40_0.tar.bz2' 'boost'
+cd ./dependencies/boost && patch -p1 < ../../patches/boost_1_40_0_thread.patch && cd ../../
+download 'https://dl.dropboxusercontent.com/u/13148925/hammmer-bootstrap-libs/' 'libantlr3c-3.1.1.tar.bz2' 'libantlr3c'
+download 'https://dl.dropboxusercontent.com/u/13148925/hammmer-bootstrap-libs/' 'boost.guid-1.40.0.tar.bz2' 'boost/guid'
+download 'https://dl.dropboxusercontent.com/u/13148925/hammmer-bootstrap-libs/' 'boost.crypto-1.40.0.tar.bz2' 'boost/crypto'
+download 'https://dl.dropboxusercontent.com/u/13148925/hammmer-bootstrap-libs/' 'boost.process-1.40.0.tar.bz2' 'boost/process'
+svn checkout -r 617 http://svn.code.sf.net/p/buildhammer/code/hammer
+
+rm -R ./build
 mkdir build
 cd build
 
-cmake ../ && cmake --build ./
+cmake ../ && make -j 4
